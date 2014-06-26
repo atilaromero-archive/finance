9c96a9ff4a8b6347f0659fa3bbc7ec514aa16cff
user <user@localhost.localdomain>
2014-06-26 20:41:30 -0300

    new file:   .hgignore 	new file:   HACKING.txt 	new file:   MANIFEST.in 	new file:   NEWS.txt 	renamed:    README.md -> README.rst 	new file:   bootstrap.py 	new file:   buildout.cfg 	new file:   extra/#option.py# 	new file:   extra/#prices.py# 	new file:   extra/COTAHIST_A2013.TXT 	new file:   extra/COTAHIST_A2014.TXT 	new file:   extra/COTAHIST_D28042014.TXT 	new file:   extra/HACKING.txt 	new file:   extra/MANIFEST.in 	new file:   extra/NEWS.txt 	new file:   extra/README.rst 	new file:   extra/bootstrap.py 	new file:   extra/buildout.cfg 	new file:   extra/setup.py 	new file:   extra/test.data 	new file:   setup.py 	new file:   src/finance/__init__.py 	renamed:    blackscholes.py -> src/finance/blackscholes.py 	renamed:    bmf.py -> src/finance/bmf.py 	renamed:    bmf2.py -> src/finance/bmf2.py 	renamed:    bmfutils.py -> src/finance/bmfutils.py 	renamed:    daily.py -> src/finance/daily.py 	renamed:    days.py -> src/finance/days.py 	renamed:    downloadquotes.sh -> src/finance/downloadquotes.sh 	renamed:    fit.py -> src/finance/fit.py 	renamed:    formulas.txt -> src/finance/formulas.txt 	renamed:    graph2.py -> src/finance/graph2.py 	renamed:    indice.txt -> src/finance/indice.txt 	renamed:    obv.py -> src/finance/obv.py 	renamed:    option.py -> src/finance/option.py 	renamed:    params.py -> src/finance/params.py 	renamed:    prices.py -> src/finance/prices.py 	renamed:    stats.py -> src/finance/stats.py 	renamed:    timegraph.py -> src/finance/timegraph.py 	renamed:    utils.py -> src/finance/utils.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '0.1'
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+]
+
+
+setup(name='finance',
+    version=version,
+    description="finance indexes in python",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='finance python blackscholes options',
+    author='Atila Leites Romero',
+    author_email='atilaromero@gmail.com',
+    url='http://github.com/atilaromero/finance',
+    license='GNU AFFERO GENERAL PUBLIC LICENSE',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            ['finance=finance:main']
+    }
+)
