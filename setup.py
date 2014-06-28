157a9e62e1dac59191703e11473ba4a42f85e610
user <user@localhost.localdomain>
2014-06-28 19:16:41 -0300

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,55 @@
+'''
+finance: finance indexes in python
+
+Note that "python setup.py test" invokes pytest on the package. With appropriately
+configured setup.cfg, this will check both xxx_test modules and docstrings.
+
+Copyright 2014, Atila Romero.
+Licensed under GNU AFFERO GENERAL PUBLIC LICENSE.
+'''
+import sys
 from setuptools import setup, find_packages
-import sys, os
+from setuptools.command.test import test as TestCommand
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.rst')).read()
-NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+# This is a plug-in for setuptools that will invoke py.test
+# when you run python setup.py test
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest  # import here, because outside the required eggs aren't loaded yet
+        sys.exit(pytest.main(self.test_args))
 
 
-version = '0.1'
+version = "0.1"
 
-install_requires = [
-    # List your project dependencies here.
-    # For more details, see:
-    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
-]
-
-
-setup(name='finance',
-    version=version,
-    description="finance indexes in python",
-    long_description=README + '\n\n' + NEWS,
-    classifiers=[
-      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    ],
-    keywords='finance python blackscholes options',
-    author='Atila Leites Romero',
-    author_email='atilaromero@gmail.com',
-    url='http://github.com/atilaromero/finance',
-    license='GNU AFFERO GENERAL PUBLIC LICENSE',
-    packages=find_packages('src'),
-    package_dir = {'': 'src'},include_package_data=True,
-    zip_safe=False,
-    install_requires=install_requires,
-    entry_points={
-        'console_scripts':
+setup(name="finance",
+      version=version,
+      description="finance indexes in python",
+      long_description=open("README.rst").read(),
+      classifiers=[ # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python'
+      ],
+      keywords="finance python blackscholes options", # Separate with spaces
+      author="Atila Romero",
+      author_email="atilaromero@gmail.com",
+      url="http://github.com/atilaromero/finance",
+      license="GNU AFFERO GENERAL PUBLIC LICENSE",
+      packages=find_packages(exclude=['examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      tests_require=['pytest'],
+      cmdclass={'test': PyTest},
+      
+      # TODO: List of packages that this one depends upon:   
+      install_requires=[],
+      # TODO: List executable scripts, provided by the package (this is just an example)
+      entry_points={
+        'console_scripts': 
             ['finance=finance:main']
-    }
+      }
 )
