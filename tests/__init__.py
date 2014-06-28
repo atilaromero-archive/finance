4d3ce1df6c631284c7ab16de69ea4f4d0fe668d7
user <user@localhost.localdomain>
2014-06-28 19:17:12 -0300

    new file:   .travis.yml 	new file:   CHANGELOG.txt 	new file:   setup.cfg 	new file:   tests/__init__.py 	new file:   tests/basic_test.py

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+'''
+finance: tests module.
+
+Meant for use with py.test.
+Organize tests into files, each named xxx_test.py
+Read more here: http://pytest.org/
+
+Copyright 2014, Atila Romero
+Licensed under GNU AFFERO GENERAL PUBLIC LICENSE
+'''
---
+++ tests/basic_test.py
@@ -0,0 +1,13 @@
+'''
+finance: Test module.
+
+Meant for use with py.test.
+Write each test as a function named test_<something>.
+Read more here: http://pytest.org/
+
+Copyright 2014, Atila Romero
+Licensed under GNU AFFERO GENERAL PUBLIC LICENSE
+'''
+
+def test_example():
+    assert True
