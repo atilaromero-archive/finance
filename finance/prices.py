8286027ba0779cc1dded06268f23ee3f8c80e7e6
atila.alr <atila.alr@z820-alr.localdomain>
2014-07-18 16:40:58 -0300

    modified:   prices.py

--- finance/prices.py
+++ finance/prices.py
@@ -1,62 +1,68 @@
 #!/usr/bin/env python
+import sys
 import option
 import blackscholes as bs
 import datetime
 import numpy as np
-
-minv=15.0
-maxv=17.3
-vl=0.018
-vh=0.020
-mes=7
-mytype='p'
+import plac
 
 strikes={
-    8.16: 50 ,
-    8.36: 52 ,
-    8.56: 94 ,
-    8.76: 96 ,
-    8.96: 98 ,
-    9.16: 10 ,
-    9.41: 80 ,
-    9.66: 40 ,
-    10.16: 11 ,
-    10.66: 41 ,
-    11.16: 12 ,
-    11.66: 42 ,
-    12.16: 13 ,
-    12.66: 43 ,
-    12.91: 83 ,
-    13.16: 14 ,
-    13.66: 44 ,
-    14.16: 15 ,
-    14.66: 45 ,
-    15.16: 16 ,
-    15.66: 46 ,
-    16.16: 7 ,
-    16.66: 47 ,
-    17.16: 18 ,
-    17.66: 48 ,
-    17.91: 82 ,
-    18.16: 9 ,
-    18.66: 49 ,
-    19.16: 20 ,
-    19.41: 81 ,
-    19.66: 89 ,
-    20.16: 21 ,
-    20.66: 90 ,
-    21.16: 92 ,
-    21.66: 91 ,
-    22.16: 93 ,
-    22.66: 72 ,
-    23.16: 24 ,
-    23.66: 73 ,
-    24.16: 54 ,
-    25.16: 26 ,
-    26.16: 56 ,
-    28.16: 28 ,
-    30.16: 31 ,
-    32.91: 33 ,
+    92: 8.40 ,
+    94: 8.60 ,
+    96: 8.80 ,
+    98: 9.00 ,
+    30: 9.50 ,
+    10: 10.00 ,
+    60: 10.25 ,
+    31: 10.50 ,
+    12: 11.00 ,
+    61: 11.25 ,
+    5: 11.50 ,
+    13: 12.00 ,
+    43: 12.75 ,
+    14: 13.25 ,
+    34: 13.50 ,
+    42: 13.75 ,
+    15: 14.25 ,
+    35: 14.50 ,
+    45: 14.75 ,
+    4: 15.50 ,
+    16: 16.00 ,
+    17: 16.50 ,
+    47: 17.00 ,
+    18: 17.50 ,
+    48: 18.00 ,
+    19: 18.50 ,
+    99: 19.00 ,
+    69: 19.25 ,
+    1: 19.50 ,
+    20: 20.00 ,
+    21: 20.50 ,
+    51: 21.00 ,
+    22: 21.50 ,
+    53: 21.75 ,
+    52: 22.00 ,
+    23: 22.50 ,
+    63: 23.00 ,
+    24: 23.50 ,
+    55: 23.75 ,
+    54: 24.00 ,
+    25: 24.50 ,
+    65: 25.00 ,
+    2: 25.50 ,
+    26: 26.00 ,
+    27: 26.50 ,
+    57: 27.00 ,
+    6: 27.25 ,
+    28: 28.00 ,
+    8: 28.25 ,
+    29: 29.00 ,
+    9: 29.25 ,
+    3: 29.50 ,
+    70: 30.00 ,
+    11: 30.25 ,
+    71: 31.00 ,
+    64: 32.75 ,
 }
 
 vencimentos={
@@ -74,13 +80,19 @@ vencimentos={
     datetime.date(2014,12,15):('L','X'),
 }
 
-if __name__=="__main__":
+def main(mytype='c', mes=8, price=20.60, minv=20, maxv=23, vl=0.018, vh=0.020):
+    price=float(price)
+    minv=float(minv)
+    maxv=float(maxv)
+    vl=float(vl)
+    vh=float(vh)
+    mes=int(mes)
     r=option.Option.rate
     venc=sorted(vencimentos.keys())[mes-1]
     Xs=[x for x in sorted(strikes.keys()) if minv<=x and x<=maxv]
     print str(venc)+'\t'+'\t'.join([str(strikes[x]) for x in Xs])
     print '%1.3f-%1.3f'%(vl,vh)+'\t'+'\t'.join([str(x) for x in Xs])
-    for S in np.arange(maxv,minv-0.001,-0.05):
+    for S in np.arange(price+1,price-1,-0.05):
         print '%1.2f'%S,
         for X in Xs:
             T=(venc-datetime.date.today()).days
@@ -94,3 +106,7 @@ if __name__=="__main__":
                 h=bs.BlackScholes(mytype,S,X,T,r,vh)
                 print '\t%02.2f-%02.2f'%(l,h),
         print
+
+if __name__=="__main__":
+    plac.call(main,sys.argv[1:])
+
